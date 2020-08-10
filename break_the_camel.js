2caf2de59b76aaf0c3ce6520fafd02b94ff34e6c
Chang Ge <gechang90@gmail.com>
2020-08-09 22:07:27 -0400

    regular expressions

--- break_the_camel.js
+++ break_the_camel.js
@@ -1,9 +1,11 @@
+
+// //replace functions
 function solution(string) {
     return string.replace(/([a-z])([A-Z])/g, "$1 $2");
 
-}
+// }
 
-
+// // filter funtions
 
 function arrayDiff(a, b) {
     function reduced(arr1, bEl) {
@@ -15,4 +17,35 @@ function arrayDiff(a, b) {
     return a;
 }
 
-// console.log(arrayDiff([1, 2, 3], [3,2]));
+// // console.log(arrayDiff([1, 2, 3], [3,2]));
+
+function capitals(word) {
+
+    const output = [];
+    arrword = word.split("");
+
+    arrword.forEach((letter, index) => {
+        if (letter.match(/[A-Z]/)) {
+            output.push(index);
+        }
+    });
+    return output;
+};
+
+// // console.log(capitals('CodEWaRs'));
+
+
+// regular expressions
+function friend(friends) {
+    //your code here
+    console.log(friends);
+
+    const friendlist = friends.filter(name => name.length == 4 && name.match(/[a-z]/gi));
+    return friendlist;
+
+}
+
+console.log(friend(["Ryan", "Kieran", "Jason", "1234"]));
+
+
+// && (/[a-z]/gi).includes(name.split()))

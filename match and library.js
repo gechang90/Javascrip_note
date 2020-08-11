73f4897b43d47abaa4640ba4910626b4950bd70d
Chang Ge <gechang90@gmail.com>
2020-08-10 22:51:51 -0400

    rename file and add switch function

--- match and library.js
+++ match and library.js
@@ -115,4 +115,14 @@ function find_average(array) {
   var sum = array.reduce((a, b) => a + b, 0);
   return sum/array.length;
 }
-
+// swith functions 
+function likes(names) {
+    names = names || [];
+    switch(names.length){
+      case 0: return 'no one likes this'; break;
+      case 1: return names[0] + ' likes this'; break;
+      case 2: return names[0] + ' and ' + names[1] + ' like this'; break;
+      case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
+      default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
+    }
+  }
